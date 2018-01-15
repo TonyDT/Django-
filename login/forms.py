97afccf5dd3c1ede4db6c878b93ab027a0344712
kongcheng <dongtaocom@126.com>
2018-01-15 23:35:10 +0800

    1.更新注册 登录 密码加密 2.注册视图明日查看修改

--- login/forms.py
+++ login/forms.py
@@ -1,5 +1,20 @@
-from django import  forms
-
+from django import forms
+from captcha.fields import CaptchaField
 class UserForm(forms.Form):
     username = forms.CharField(label="用户名",max_length=128,widget=forms.TextInput(attrs={'class':'form-control',"placeholder":"请输入用户名"}))
     password = forms.CharField(label="密   码",max_length=256,widget=forms.PasswordInput(attrs={'class':'form-control',"placeholder":"请输入密码"}))
+    captcha = CaptchaField(label='验证码')
+
+class RegisterForm(forms.Form):
+    gender = (
+        ('male',"男"),
+        ('female',"女"),
+    )
+    username = forms.CharField(label="用户名",max_length=128,widget=forms.TextInput(attrs={'class':'form-control','placeholder':"请输入用户名"}))
+    password1 = forms.CharField(label="密   码", max_length=256,
+                               widget=forms.PasswordInput(attrs={'class': 'form-control', "placeholder": "请输入密码"}))
+    password2 = forms.CharField(label="密   码", max_length=256,
+                               widget=forms.PasswordInput(attrs={'class': 'form-control', "placeholder": "请再次输入密码"}))
+    email = forms.EmailField(label="邮箱地址", widget=forms.EmailInput(attrs={'class': 'form-control'}))
+    sex = forms.ChoiceField(label='性别', choices=gender)
+    captcha = CaptchaField(label='验证码')
--- mys/settings.py
+++ mys/settings.py
@@ -39,8 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'login',
+    'captcha',
 ]
-
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
